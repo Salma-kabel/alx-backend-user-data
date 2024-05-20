81d58e93f9ff1051a863a4829e5cbbfa4ebc63a4
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-20 17:24:30 +0000

    task3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,17 +14,15 @@ class Auth:
         """returns path and excluded_paths"""
         if path is None:
             return True
-        if path in excluded_paths or (path + '/') in excluded_paths :
+        if path in excluded_paths or (path + '/') in excluded_paths:
             return False
         return True
-
 
     def authorization_header(self, request=None) -> str:
         """ Flask request object"""
         if request is None:
             return None
         return request.headers.get("Authorization")
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """will be the Flask request object"""
