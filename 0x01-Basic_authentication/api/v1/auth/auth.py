9807ee9d60cff0a664d94c1b8a2ab3bc161dd433
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-20 17:11:47 +0000

    task1

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""class to manage the API authentication"""
+
+
+from flask import request
+from tabnanny import check
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """class to manage api"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns path and excluded_paths"""
+        if path is None:
+            return True
+        if path in excluded_paths or (path + '/') in excluded_paths :
+            return False
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Flask request object"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """will be the Flask request object"""
+        return None
