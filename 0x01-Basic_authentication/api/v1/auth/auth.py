ef4a3b6aa220bfa3d9dfc20d2f6d1518fa81771e
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-20 17:29:34 +0000

    task3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,9 @@ from typing import TypeVar, List
 
 
 class Auth:
-    """class to manage api"""
+    """class to manage api auth"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns path and excluded_paths"""
+        """returns none-path and excluded_paths"""
         if path is None:
             return True
         if path in excluded_paths or (path + '/') in excluded_paths:
@@ -19,11 +19,11 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Flask request object"""
+        """Flask request object return none-header"""
         if request is None:
             return None
         return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """will be the Flask request object"""
+        """will be the Flask request object return none"""
         return None
