6c74b4f09e19b93a5a96cf187e64e51f935e3151
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-21 10:07:29 +0000

    task9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,14 +33,14 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-        def extract_user_credentials(self,
-                decoded_base64_authorization_header: str) -> (str, str):
-            """ returns the user email and password
-            from the Base64 decoded value"""
-            if (decoded_base64_authorization_header is None or
-                    type(decoded_base64_authorization_header) is not str
-                    or ':' not in decoded_base64_authorization_header):
-                return (None, None)
-            headr = decoded_base64_authorization_header
-            return (headr.split(":")[0],headr.split(":")[1])
+    def extract_user_credentials(self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """ returns the user email and password
+        from the Base64 decoded value"""
+        if (decoded_base64_authorization_header is None or
+                type(decoded_base64_authorization_header) is not str
+                or ':' not in decoded_base64_authorization_header):
+            return (None, None)
+        headr = decoded_base64_authorization_header
+        return (headr.split(":")[0],headr.split(":")[1])
             
