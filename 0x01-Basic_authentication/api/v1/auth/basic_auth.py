92078762952d12860d6d3c86d99611e62b2cf40e
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-21 09:21:53 +0000

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,7 +25,7 @@ class BasicAuth(Auth):
         """returns the decoded value of a Base64
         string base64_authorization_header"""
         if (base64_authorization_header is None or
-                type(base64_authorization_header) is not str:
+                type(base64_authorization_header) is not str):
             return None
         try:
             return base64.b64decode(base64_authorization_header).decode('utf-8')
