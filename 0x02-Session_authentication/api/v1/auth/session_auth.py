b29167d7aa23202708b2731ce054a8b5dae2e5ee
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-23 11:01:10 +0000

    task2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,7 @@ import uuid
 class SessionAuth(Auth):
     """ the first step for creating a new authentication mechanism"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """creates a Session ID for a user_id"""
         if user_id is None or type(user_id) is not str:
@@ -20,4 +21,3 @@ class SessionAuth(Auth):
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
-
