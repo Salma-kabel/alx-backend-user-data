c0c59c7ebcb8c2aacd6047ca19a3c6ec1593da29
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-23 16:47:13 +0000

    task9

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -20,7 +21,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
-if getenv("AUTH_TYPE") == "session_auth":
+if getenv("AUTH_TYPE") == "session_exp_auth":
+    auth = SessionExpAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
     auth = SessionAuth()
 elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""session expiration auth"""
+
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
+from uuid import uuid4
+from models.user import User
+from os import getenv
+
+
+class SessionExpAuth(SessionAuth):
+    """add an expiration date to a Session ID"""
+    def __init__(self):
+        """Assign an instance attribute session_duration"""
+        self.session_duration = int(getenv('SESSION_DURATION', 0))
+
+    def create_session(self, user_id=None):
+        """Create a Session ID by calling super()"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {"user_id": user_id, "created_at": datetime.now()}
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """Return None if session_id is None"""
+        if session_id is None:
+            return None
+        if self.user_id_by_session_id.get(session_id):
+            user_id = session_dictionary.get('user_id')
+            if self.session_duration <= 0:
+                return user_id
+            created = self.session_dictionary.get(created_at)
+            if created:
+                if created + timedelta(seconds=self.session_duration) < datetime.now():
+                    return None
+                return user_id
+        return None
