e877227b85f32bfbc9c1155feec49142f7a85ba0
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-23 10:55:54 +0000

    task2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,7 +6,18 @@ from api.v1.auth.auth import Auth
 from typing import TypeVar, List
 from models.user import User
 import base64
+import uuid
 
 
 class SessionAuth(Auth):
     """ the first step for creating a new authentication mechanism"""
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+        else:
+            session_id = kwargs.get('id', str(uuid.uuid4()))
+            user_id_by_session_id[session_id] = user_id
+            return session_id
+
