7a26d14eab3636488ef4bf47e637277063387322
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-23 11:27:31 +0000

    task5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,20 +53,6 @@ def before_request():
     arr = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
             '/api/v1/auth_session/login/']
     if auth and auth.require_auth(request.path, arr):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
-        request.current_user = auth.current_user(request)
-        if auth.authorization_header(request) is None and
-        auth.session_cookie(reque@app.before_request
-
-
-def before_request():
-    """handler before_request"""
-    arr = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/']
-    if auth and auth.require_auth(request.path, arr):
         if auth.authorization_header(request) is None and
         auth.session_cookie(request) is None:
             abort(401)
