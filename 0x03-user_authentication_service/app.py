31dcdfd899df573c73cae7a533d440dff3bc7ed4
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-28 10:22:11 -0700

    task6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""set up a basic Flask app"""
+
+
+from auth import Auth
+from flask import Flask, jsonify, request, abort, redirect
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
