f0fca15e5f31086959146006967a595d639ab2eb
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-28 10:16:51 -0700

    task5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,6 +14,7 @@ def _hash_password(password: str) -> str:
     """takes in a password string arguments and returns bytes"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -26,7 +27,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user:
-                raise ValueError("User "+ email + " already exists")
+                raise ValueError("User " + email + " already exists")
         except NoResultFound:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
