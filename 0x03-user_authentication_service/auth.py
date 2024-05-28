e2fc43b09dc7f8db9d7ee01a3ae6f3a1e504257a
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-28 09:58:43 -0700

    task3

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""auth file script"""
+
+
+from db import DB
+from typing import TypeVar
+from user import User
+import bcrypt
+from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> str:
+    """takes in a password string arguments and returns bytes"""
+    return bcrypt.hashpw(password.encode("uft-8"), bcrypt.gensalt())
