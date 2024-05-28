ebcafd30667f6da432038777836e3da4444c2cd7
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-28 07:55:00 -0700

    task2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,5 +49,6 @@ class DB:
             user = self._session.query(User).filter_by(**kwargs).first()
             if not user:
                 raise NoResultFound
+            return user
         except InvalidRequestError:
             raise InvalidRequestError
