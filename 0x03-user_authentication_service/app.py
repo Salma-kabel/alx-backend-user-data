db6e635cfd415ed69a8a3db6006a0bc2a45e4341
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-01 15:22:14 +0000

    task17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,17 @@ def profile() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """If the email is not registered, respond with a 403 status code
+    Otherwise, generate a token and respond with a 200 HTTP status"""
+    email = request.form.get('email')
+    sessiopn_id = AUTH.create_session(email)
+    if session_id:
+        return jsonify({"email": email, "reset_token": session_id}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
