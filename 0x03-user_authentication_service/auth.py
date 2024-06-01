ab18513f21caed3a387390fb60756b1edc562975
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-01 11:51:39 +0000

    task8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,11 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return user
+    def valid_login(self, email: str, password: str) -> bool:
+        """ expect email and password required arguments
+        and return a boolean"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+        except NoResultFound:
+            return False
