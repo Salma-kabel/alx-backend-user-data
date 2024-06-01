1ca28a3c1dd78638eb9bb3ae24cc5eb1ca3f365d
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-01 14:35:08 +0000

    task11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,5 +27,21 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+    """ implement a login function to respond
+    to the POST /sessions route"""
+def login() -> str:
+    email = request.form.get("email")
+    password = request.form.get("password")
+    valid_login = AUTH.valid_login(email, password)
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": f"{email}", "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+        return res
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
