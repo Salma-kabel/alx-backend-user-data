07e019d754b2591e425a91e9f06b52783445d1a3
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-01 14:52:56 +0000

    task12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,7 +59,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """returns the corresponding User or None"""
         if session_id is None:
             return None
