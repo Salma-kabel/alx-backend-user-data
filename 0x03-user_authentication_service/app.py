b611dc60f61b86c100985d97fd55cd4375ff4b29
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-01 16:17:51 +0000

    task19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,20 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """Update the password. If the token is invalid,
+    catch the exception and respond with a 403 HTTP code"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({"email": email,
+                        "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
