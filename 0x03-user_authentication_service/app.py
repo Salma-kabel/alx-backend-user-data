64a3f9eb50c5898e6eba875dd82a93dded4f6183
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-01 15:09:52 +0000

    task15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,17 @@ def logout() -> str:
         abort(403)
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """ find the user. If the user exist, respond with a
+    200 HTTP status and the following JSON payload"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
